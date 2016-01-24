1ddbe1a906e8d8c34b3ecf52b33d2e9ed74009e7
Justin Mullis <justin.mullis@gmail.com>
2016-01-23 20:44:47 -0500

    password confirmation

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 "use strict";
 
-$( document ).ready( function(){
+var toolTip = function(obj, tipText, tipID) {
+  var tipParent = obj.closest(".form-group").children(".tooltip");
+  var tipDiv = tipParent.children(("#" + tipID));
 
-  var maxLength = function(obj, val) {
-    var toolTip = $(obj).closest(".form-group").children(".tooltip");
-    var maxLengthText = (val - obj.value.length) + " characters remaining";
-    var maxLengthTip = toolTip.children("#max-length");
-
-    if ( obj.value.length > 0 ) {
-      
-      if ( maxLengthTip[0] ) {
-        maxLengthTip.text(function () {
-          return maxLengthText;
-        });
-      } else {
-        toolTip.append("<div/>").find("div:last").attr("id", "max-length").text(function () {
-          return maxLengthText;
-        });
-      }
-      
+  if ( obj.val().length > 0 ) {
+    
+    if ( tipDiv[0] ) {
+      tipDiv.text(tipText);
     } else {
-      maxLengthTip.remove();
+      tipParent.append("<div/>").find("div:last")
+                  .attr("id", tipID).text(tipText);
     }
-  };
+    
+  } else {
+    tipDiv.remove();
+  }
+};
+
+var maxLength = function(obj, val) {
+  var maxLengthText = (val - obj.val().length) + " characters remaining";
+  toolTip(obj, maxLengthText, "max-length");
+};
+
+
+var passwordMatch = function(confirm_field, password_field) {
+  if (confirm_field.val() !== password_field.val()) {
+    toolTip(confirm_field, "confirmation does not match", "pw-confirm");
+  } else {
+    $("#pw-confirm").remove();
+  }
+};
+
+$( document ).ready( function(){
+  //event handlerz
 
   $("#text-input").on("input", function() {
-    maxLength(this, 32);
+    maxLength($(this), 32);
   });
 
   $("#textarea-input").on("input", function() {
-    maxLength(this, 140);
+    maxLength($(this), 140);
   });
 
   $("#password-input").on("input", function() {
-    maxLength(this, 16);
+    maxLength($(this), 16);
   });
 
   $("#password-confirmation").on("input", function() {
-    maxLength(this, 16);
+    maxLength($(this), 16);
+    passwordMatch($(this), $("#password-input"));
   });
 
 });
