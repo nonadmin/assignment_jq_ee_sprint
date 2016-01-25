7b25b53f13f852a71b66db12a4267bb7169dee77
Justin Mullis <justin.mullis@gmail.com>
2016-01-24 19:26:29 -0500

    photo tagging refactor

--- photo_tagging.js
+++ photo_tagging.js
@@ -2,7 +2,7 @@
 var friends = ["Leah", "Biggie", "Justin"];
 
 $( document ).ready( function(){
-  var tagBox = function(){
+  var newTagBox = function(){
     return $("<div>").attr({class: "tag-box tag-active", id: "active"});
   };
 
@@ -18,9 +18,8 @@ $( document ).ready( function(){
 
   $(".photo-container").hover( 
     function(){
-      $(this).append(tagBox());
+      $(this).append(newTagBox());
     },
-
     function(){
       $("#active").remove();
     }
@@ -30,39 +29,34 @@ $( document ).ready( function(){
     $("#active").offset({top: (event.pageY - 50), left: (event.pageX - 50)});
   });
 
-
-  $(".photo-container").click( function(){
+  $(".photo-container").on("click", function(){
     $("#active").attr({"id": "pending", "class": "tag-box tag-set"})
                     .append(function() {
       return $("<div>").addClass("friend-list").append(friendList());
     });
   });
 
+  // li events, delegated
   $(".photo-container").on({
     mouseenter: function (event) {
-      $(event.target).addClass("active");
+      $(event.target).addClass("li-active");
     },
     mouseleave: function (event) {
-      $(event.target).removeClass("active");
+      $(event.target).removeClass("li-active");
     },
     click: function(event) {
       event.stopImmediatePropagation();
       
       $("#pending").attr({"id": ""});
       $(event.target).parent().children().not(event.target).remove();
-      $(event.target).attr("class", "set");
-      $(".photo-container").append(tagBox());
+      $(event.target).attr("class", "li-set");
+      $(".photo-container").append(newTagBox());
     }
   }, "li");
 
+  // delegate pending div events
   $(".photo-container").on("mouseleave", "#pending", function(){
     $("#pending").remove();
   });
-
-
-
-      // $("#pending").mouseleave(function(){
-    //   $(this).replaceWith(tagBox());
-    // });
 
 });
